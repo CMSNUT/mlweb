f0ebc4ca94b406293308f1e2ced2bcb6470b3440
Aikemi <dhxia@snut.edu.cn>
2026-01-28 21:57:19 +0800

    init

--- run.bat
+++ run.bat
@@ -16,10 +16,10 @@ set "RSCRIPT_452_PATH=D:\R\R-4.5.2\bin\x64\Rscript.exe" :: Rscript路径
 set "PY_310_ENV=py310"                     :: Python子服务环境名
 :: set "PY_310_SCRIPT=app.py"                 :: Python子服务启动脚本
 
-echo 启动R服务(%R_452_NAME%)...
-start "R452-Service (%R_452_NAME%)" cmd /k "cd /d "%R_452_PATH%" && "%RSCRIPT_452_PATH%" %R_452_SCRIPT% && pause"
+@REM echo 启动R服务(%R_452_NAME%)...
+@REM start "R452-Service (%R_452_NAME%)" cmd /k "cd /d "%R_452_PATH%" && "%RSCRIPT_452_PATH%" %R_452_SCRIPT% && pause"
 
 echo 启动Python主后端(%PY_MAIN_ENV%)...
-start "Python-Main" cmd /k "echo 正在启动Python服务... && echo 激活conda环境... && call "%CONDA_PATH%\Scripts\activate.bat" %PY_310_ENV% && streamlit run app.py && pause"
+start "Python-Main" cmd /k "echo 正在启动Python服务... && echo 激活conda环境... && call "%CONDA_PATH%\Scripts\activate.bat" %PY_310_ENV%  && cd /d backend && streamlit run main.py && pause"
 
 :: streamlit run app.py
